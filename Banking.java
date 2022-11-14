a2e6c6d9589ff84362f79e2c53473d6cf1ad0ace
Hammad <hammadsiddiqui1212@gmail.com>
2022-11-14 11:13:07 +0530

    final commit

--- Banking.java
+++ Banking.java
@@ -2,18 +2,25 @@ class Banking{
     String name;
     int age;
     long aadhar;
+    double balance;
+    public double withdraw(double balance){
+     System.out.println("Enter amount to withdraw ")
+     amount=sc.nextDouble();
+     if(amount>balance)
+        System.out.println("Insufficient balance")
+    else
+        balance-balance-amount;
+    return balance;
 
-    public double withdraw(){
-        double balance;
+    }
 
+    public double deposit(double balance){
+        System.out.println("Enter amount to deposit ")
+     amount=sc.nextDouble();
+     balance-balance+amount;
 
         return balance;
     }
-
-    public double deposit(){
-        double balance;
-
-        return balance;
-    }
+    void
 
 }
